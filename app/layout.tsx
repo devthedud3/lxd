836a9423a3099d41f21ebd9ce3e9229e3b7a73d2
corey <corey.cushnie@gmail.com>
2024-04-11 21:52:18 -0400

    poppins font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import { inter } from "./fonts";
+import "./ui/globals.css";
+import { poppins } from "./ui/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
