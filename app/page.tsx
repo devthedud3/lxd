96fbfcfdab48c43b506b135ff00815e574836a51
corey <corey.cushnie@gmail.com>
2024-04-12 14:47:45 -0400

    optimize

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import { Banner, Button, Header } from "./ui/components";
 import { FiArrowUpRight } from "react-icons/fi";
+import ImageCard from "./ui/components/image-card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen h-screen flex-col items-center p-0">
-      <Banner message="Free shipping for our first 200 orders" />
+      <Banner message="🎉🎉 Free shipping for our first 200 orders! 🎉🎉" />
       <div className="flex flex-col h-full w-full px-10 pb-4">
         <Header />
-        <div className="flex h-full">
-          <div className="flex flex-1 flex-col justify-between px-3 mr-3">
+        <div className="flex h-full items-center justify-between w-full flex-wrap">
+          <div className="flex w-fit flex-col justify-between px-3 mr-3 ">
             <div className="flex flex-col w-full py-10 ">
-              <div className="flex w-full justify-between">
-                <p className="text-5xl mb-4 leading-tight font-bold">
+              <div className="block w-fit">
+                <p className="text-5xl mb-4 leading-tight font-bold ">
                   FASHION FUSION
                   <br /> WEAR YOUR MASTERPIECE
                 </p>
@@ -26,42 +27,33 @@ export default function Home() {
                 <Button name="Learn More" />
               </div>
             </div>
-            <div className="flex text-white space-x-4">
-              <div className="relative">
-                <Image
-                  className="block"
-                  src="/homepage/seasonal.png"
-                  width={400}
-                  height={400}
-                  alt=""
-                />
-                <div className="absolute inset-0 flex items-end justify-center p-4 ">
-                  Test
-                </div>
-              </div>
-              <div className="relative">
-                <Image
-                  className="block"
-                  src="/homepage/essentials.png"
-                  width={400}
-                  height={400}
-                  alt=""
-                />
-                <div className="absolute inset-0 flex items-end justify-center p-4 ">
-                  Test
-                </div>
-              </div>
+            <div className="flex text-white items-center justify-center flex-wrap">
+              <ImageCard
+                src="/homepage/seasonal.png"
+                size={400}
+                headline="seasonal trends"
+                details="Discover Your Signature Style Unleash your Boldness. Where Trends Meet Timeless"
+                animate
+              />
+              <ImageCard
+                src="/homepage/essentials.png"
+                size={400}
+                headline="the essentials"
+                details="Discover Your Signature Style Unleash your Boldness. Where Trends Meet Timeless"
+                animate
+              />
             </div>
           </div>
-          <div className="relative w-fit">
+
+          <div className="relative flex w-fit justify-center">
             <Image
-              className="block"
+              className="block p-3"
               src="/homepage/trending.png"
               width={510}
               height={10}
               alt=""
             />
-            <div className="absolute inset-0 flex items-end justify-center p-4 ">
+            <div className="absolute inset-0 flex items-end justify-center p-6 ">
               <div className="flex flex-col p-10 bg-white border w-full justify-between">
                 <div className="flex w-full justify-between">
                   <p className="text-2xl mb-4 leading-tight font-bold">
