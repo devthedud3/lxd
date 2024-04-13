8bc4399972b1583e2728c6f42b44ebf943e9cfb5
corey <corey.cushnie@gmail.com>
2024-04-13 12:14:14 -0400

    banner details

---
+++ app/lib/constants.ts
@@ -0,0 +1,7 @@
+export const BANNER_CATEGORIES = {
+  shipping_discount: "🎉🎉 Free shipping for our first 200 orders! 🎉🎉",
+  discount_20: "🧭🧭 20% Discount on all orders this month! 🧭🧭",
+  discount_35: "🧭🧭 35% Discount on all orders this month! 🧭🧭",
+  discount_40: "🧭🧭 40% Discount on all orders this month! 🧭🧭",
+  discount_50: "🧭🧭 50% Discount on all orders this month! 🧭🧭",
+};
