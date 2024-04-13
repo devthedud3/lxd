b72f5c9ad6423ab3c9189ad7558c7ebd31a2bff8
corey <corey.cushnie@gmail.com>
2024-04-13 12:15:13 -0400

    banner

---
+++ app/ui/components/banner.tsx
@@ -0,0 +1,18 @@
+import { BANNER_CATEGORIES } from "@/app/lib/constants";
+import React from "react";
+
+type BannerProps = {
+  show: boolean;
+};
+
+export default function Banner({ show }: BannerProps) {
+  return (
+    <>
+      {show && (
+        <div className="fixed text-xs w-full bg-stone-900 text-white py-2 text-center">
+          {BANNER_CATEGORIES.shipping_discount.toLocaleUpperCase()}
+        </div>
+      )}
+    </>
+  );
+}
