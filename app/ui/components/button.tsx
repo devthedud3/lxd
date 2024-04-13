7aabb33e08b9685b8803e0e188d224427db925aa
corey <corey.cushnie@gmail.com>
2024-04-13 12:15:23 -0400

    update btn

--- app/ui/components/button.tsx
+++ app/ui/components/button.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 type ButtonProps = {
@@ -6,14 +9,28 @@ type ButtonProps = {
   primary?: boolean;
   //@ts-ignore
   icon?: IconType;
+  route?: string;
 };
 
-export default function Button({ name, animate, primary, icon }: ButtonProps) {
+export default function Button({
+  name,
+  animate,
+  primary,
+  icon,
+  route,
+}: ButtonProps) {
+  const router = useRouter();
   const color = primary && "bg-amber-300 border-amber-300 ";
+  const a = animate && "hover:scale-105 ";
+
+  function handleClick() {
+    route && router.push(route);
+  }
   return (
     <>
       <div
-        className={`transition duration-100 ease-in hover:scale-105 hover:border-black flex border ${color} h-fit p-3 cursor-pointer`}
+        className={`transition duration-100 ease-in ${a} hover:border-black flex border ${color} h-fit p-3 cursor-pointer`}
+        onClick={handleClick}
       >
         {icon && icon}
         {name && <p className="text-sm text-nowrap px-8">{name}</p>}
