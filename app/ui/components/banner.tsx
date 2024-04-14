ca1976bbb53d63efd7afe18f24f0ab82ac4613ca
corey <corey.cushnie@gmail.com>
2024-04-14 02:36:02 -0400

    fix banner

--- app/ui/components/banner.tsx
+++ app/ui/components/banner.tsx
@@ -9,7 +9,7 @@ export default function Banner({ show }: BannerProps) {
   return (
     <>
       {show && (
-        <div className="fixed text-xs w-full bg-stone-900 text-white py-2 text-center">
+        <div className="fixed text-xs w-full bg-stone-900 text-white py-2 text-center z-40">
           {BANNER_CATEGORIES.shipping_discount.toLocaleUpperCase()}
         </div>
       )}
