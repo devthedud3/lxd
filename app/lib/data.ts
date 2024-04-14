e487c673a02aa6f6a210eb82d83abb8387b2ab4e
corey <corey.cushnie@gmail.com>
2024-04-14 11:04:03 -0400

    test

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,10 @@
+let url =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:3000"
+    : process.env.POSTGRES_URL;
+
 export const getShop = async () => {
-  const response = await fetch(`http://localhost:3000/api/shop`, {
+  const response = await fetch(`${url}/api/shop`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -12,7 +17,7 @@ export const getShop = async () => {
 };
 
 export const getItemById = async (query: string) => {
-  const response = await fetch(`http://localhost:3000/api/shop/${query}`, {
+  const response = await fetch(`${url}/api/shop/${query}`, {
     method: "GET",
     headers: {
       cache: "no-store",
