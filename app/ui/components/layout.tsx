0922415a505616d5d13d422b49f28de871d099af
corey <corey.cushnie@gmail.com>
2024-04-14 02:37:30 -0400

    create layout component

---
+++ app/ui/components/layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./navbar";
+import Banner from "./banner";
+
+interface LayoutInterface {
+  children: React.ReactNode;
+  padded?: boolean;
+}
+export default function Layout({ children, padded }: LayoutInterface) {
+  return (
+    <>
+      <Banner show />
+      <div className="flex flex-col h-full w-full justify-between items-center">
+        <div className="w-full mt-10 px-10 pb-1 border-b">
+          <Navbar />
+        </div>
+        <main
+          className={`flex flex-col ${padded ? "px-32" : "w-full"} flex-wrap`}
+        >
+          {children}
+        </main>
+      </div>
+    </>
+  );
+}
