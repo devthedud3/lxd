d56e87fa6ab5da8215b235ef3e1952a07d3e0e3c
corey <corey.cushnie@gmail.com>
2024-04-14 13:27:42 -0400

    update exports

--- app/ui/components/index.ts
+++ app/ui/components/index.ts
@@ -1,7 +1,7 @@
 import Navbar from "./navbar";
 import Banner from "./banner";
 import Button from "./button";
-import Layout from "./layout";
+
 import Logo from "./logo";
 
-export { Navbar, Banner, Button, Logo, Layout };
+export { Navbar, Banner, Button, Logo };
