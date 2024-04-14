7d83779bab747569b1d2da4b6f5ca6db33117f5e
corey <corey.cushnie@gmail.com>
2024-04-14 02:34:57 -0400

    fix data file

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,23 @@
 export const getShop = async () => {
-  const response = await fetch(`${process.env.VERCEL_URL}/api/shop`);
-  console.log(response.status);
+  const response = await fetch(`http://localhost:3000/api/shop`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      cache: "no-store",
+    },
+  });
+  const { items } = await response.json();
+
+  return items;
+};
+
+export const getItemById = async (query: string) => {
+  const response = await fetch(`http://localhost:3000/api/shop/${query}`, {
+    method: "GET",
+    headers: {
+      cache: "no-store",
+    },
+  });
+  const { items } = await response.json();
+  return items;
 };
