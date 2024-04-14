2c5df5e83d80ebdcc62a47ac5aa492936cbf84c6
corey <corey.cushnie@gmail.com>
2024-04-14 12:58:02 -0400

    add fetch to component

--- app/ui/components/card-list.tsx
+++ app/ui/components/card-list.tsx
@@ -1,17 +1,37 @@
-import { getShop } from "@/app/lib/data";
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Card from "./card";
 
 interface ItemList {
   category?: string;
 }
 
-export async function CardList({ category }: ItemList) {
-  const items = await getShop();
+export function CardList({ category }: ItemList) {
+  const [shop, setShop] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const items: any = [];
+  useEffect(() => {
+    async function getShop() {
+      const response = await fetch("/api/shop", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          cache: "no-store",
+        },
+      });
+
+      const { items } = await response.json();
+      return items;
+    }
+    getShop()
+      .then((items) => setShop(items))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {items.map((val: any, key: number) => {
+      {shop.map((val: any, key: number) => {
         return <Card key={key} {...val} />;
       })}
     </div>
