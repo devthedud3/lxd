8954b091c405723a025d118eb540c7ded59a5185
corey <corey.cushnie@gmail.com>
2024-04-15 17:59:24 -0400

    update shop

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -5,23 +5,7 @@ export default function Shop() {
   return (
     <>
       <div className="flex flex-1 flex-col h-full w-full items-center">
-        <div className="flex justify-center py-20">
-          <h1 className="text-3xl ">FIND BY CATEGORY</h1>
-        </div>
-        <div className="hidden space-x-10 py-6 text-xs md:flex">
-          {SHOP_CATEGORIES.map((category, key) => {
-            return (
-              <p
-                className="w-fit transition duration-200 ease-in border-b border-white hover:border-black cursor-pointer text-nowrap"
-                key={key}
-              >
-                {category}
-              </p>
-            );
-          })}
-        </div>
-
-        <CardList />
+        <CardList headline="find by category" />
       </div>
     </>
   );
