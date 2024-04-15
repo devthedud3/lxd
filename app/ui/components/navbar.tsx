905da69d3849cc5c70eb059f0cd1759eb08edbb5
corey <corey.cushnie@gmail.com>
2024-04-15 18:00:28 -0400

    center logo

--- app/ui/components/navbar.tsx
+++ app/ui/components/navbar.tsx
@@ -6,23 +6,27 @@ import Logo from "./logo";
 export default function Navbar() {
   return (
     <div className="flex h-full justify-between w-full flex-wrap ">
-      <div className="transition ease-out duration-200 flex items-center space-x-2 hover:scale-105 cursor-pointer">
+      <div className="transition ease-out duration-200 flex items-center space-x-2 hover:scale-105 cursor-pointer flex-1">
         <HiOutlineMenuAlt3 size={20} />
         <p>Menu</p>
       </div>
+
       <Logo size={35} />
-      <div className="flex items-center space-x-6">
-        <RiSearch2Line
-          className="transition ease-out duration-200 cursor-pointer hover:scale-110"
-          size={20}
-        />
-        <RiShoppingCartLine
-          className="transition ease-out duration-200 cursor-pointer hover:scale-110"
-          size={20}
-        />
-        <p className="transition ease-out duration-200 border-b border-white hover:border-black hover:scale-105 cursor-pointer">
-          Login
-        </p>
+
+      <div className="flex-1 flex justify-end">
+        <div className="flex items-center space-x-6">
+          <RiSearch2Line
+            className="transition ease-out duration-200 cursor-pointer hover:scale-110"
+            size={20}
+          />
+          <RiShoppingCartLine
+            className="transition ease-out duration-200 cursor-pointer hover:scale-110"
+            size={20}
+          />
+          <p className="transition ease-out duration-200 border-b border-white hover:border-black hover:scale-105 cursor-pointer">
+            Login
+          </p>
+        </div>
       </div>
     </div>
   );
