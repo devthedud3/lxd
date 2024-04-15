9b735535f6d66d4ac024b3ee8003d4fcd7cf429e
corey <corey.cushnie@gmail.com>
2024-04-15 17:53:31 -0400

    create loading skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <main className="text-center">
+    <main className="flex h-screen w-full justify-center items-center">
       <h2 className="text-primary">Loading</h2>
       <p>Hopefully not too long</p>
     </main>
