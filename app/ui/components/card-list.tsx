d3414fc065e274d77303816511b19b47cce9a70d
corey <corey.cushnie@gmail.com>
2024-04-15 18:13:17 -0400

    update card list

--- app/ui/components/card-list.tsx
+++ app/ui/components/card-list.tsx
@@ -3,12 +3,11 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import Card from "./card";
-import { SHOP_CATEGORIES } from "@/app/lib/constants";
 import { getSearchQuery, getShop } from "@/app/lib/data";
+import CategoryMenu from "./categories-menu";
 
 export function CardList({ headline }: { headline: string }) {
   const [shop, setShop] = useState<any>([]);
-  const [category, setCategory] = useState<string>(SHOP_CATEGORIES[0]);
   const [query, setQuery] = useState<string>(getSearchQuery());
 
   const router = useRouter();
@@ -18,31 +17,12 @@ export function CardList({ headline }: { headline: string }) {
     getShop(query).then((items) => setShop(items));
   }, [query]);
 
-  function updateCategories(e: string) {
-    setCategory(e);
-    setQuery(`?category=${e}`);
-  }
-
   return (
     <>
       <div className="flex justify-center pt-20 pb-4">
         <h1 className="text-3xl ">{headline.toLocaleUpperCase()}</h1>
       </div>
-      <div className="hidden space-x-3 py-6 text-xs md:flex">
-        {SHOP_CATEGORIES.map((cat, key) => {
-          return (
-            <div
-              className={`w-fit transition duration-200 ease-in border-b p-3 ${
-                category === cat ? "border-black" : "border-white"
-              } hover:bg-stone-200 cursor-pointer text-nowrap`}
-              key={key}
-              onClick={() => updateCategories(cat)}
-            >
-              {cat.toLocaleUpperCase()}
-            </div>
-          );
-        })}
-      </div>
+      <CategoryMenu setQuery={setQuery} />
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-20">
         {shop.map((val: any, key: number) => {
