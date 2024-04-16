0dfe9686199b6ffa29f7cd457135e2b111e9ec0f
corey <corey.cushnie@gmail.com>
2024-04-16 12:42:25 -0400

    minor changes

--- app/ui/components/_StatusBadge.tsx
+++ app/ui/components/_StatusBadge.tsx
@@ -3,9 +3,9 @@ import { LuConstruction } from "react-icons/lu";
 
 export default function Status() {
   return (
-    <div className="border-2 border-black bg-black flex space-x-2 left-2 items-center bottom-2 p-2 fixed z-50 rounded-full">
+    <div className="border-2 border-white bg-black flex space-x-2 left-2 items-center top-2 p-2 fixed z-50 rounded-full">
       <LuConstruction color="yellow" />
-      <h1 className="text-sm text-white ">In development</h1>
+      <h1 className="text-xs text-white ">In development</h1>
       <LuConstruction color="yellow" />
     </div>
   );
