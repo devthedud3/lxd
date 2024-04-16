a0fa2b0ed027a70534b694b942daf2d5bf4d4928
corey <corey.cushnie@gmail.com>
2024-04-15 21:06:30 -0400

    fix card list

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,11 +1,10 @@
-import { SHOP_CATEGORIES } from "../lib/constants";
 import { CardList } from "../ui/components/card-list";
 
 export default function Shop() {
   return (
     <>
       <div className="flex flex-1 flex-col h-full w-full items-center">
-        <CardList headline="find by category" />
+        <CardList headline="find by category" search />
       </div>
     </>
   );
