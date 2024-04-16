393bfa56600c2edff4218fb8d360778ca9169128
corey <corey.cushnie@gmail.com>
2024-04-16 10:02:58 -0400

    add dev badge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Banner, Navbar } from "./ui/components";
+import { Banner, Navbar, Status } from "./ui/components";
 import { Suspense } from "react";
 
 import { poppins } from "./ui/fonts";
@@ -21,13 +21,14 @@ export default function RootLayout({
         className={`${poppins.className} flex flex-col h-screen items-center`}
         suppressHydrationWarning
       >
+        <Status />
         <Banner show />
         <Suspense fallback={<div>...Loading</div>} />
         <div className="w-full mt-10 px-10 pb-1 border-b">
           <Navbar />
         </div>
         <main
-          className={`flex flex-col p-2 md:px-32 flex-wrap justify-center items-center`}
+          className={`flex flex-col p-2 md:px-32 flex-wrap  justify-center items-center`}
         >
           {children}
         </main>
