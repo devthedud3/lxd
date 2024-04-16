164b96a2c25002c2b299868265d2c8ff4f580112
corey <corey.cushnie@gmail.com>
2024-04-16 10:03:16 -0400

    build page

--- app/shop/mens/page.tsx
+++ app/shop/mens/page.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-import { CardSlider } from "@/app/ui/components/card-slider";
+import { Button, CardSlider, List, ProductDetails } from "@/app/ui/components";
+import { SHIPPING_DETAILS } from "@/app/lib/constants";
+import { ShopAPI } from "@/app/lib/data";
 
 export default function Mens() {
-  return <CardSlider />;
+  const [product, setProduct] = useState<Item>();
+  useEffect(() => {
+    ShopAPI().then((item) => setProduct(item[0]));
+  }, []);
+
+  return (
+    <div className="flex flex-col h-full w-full justify-between">
+      <div className="mb-10 flex space-x-4 overflow-x-scroll">
+        <CardSlider image={product?.image_url} />
+        <CardSlider image={product?.image_url} />
+        <CardSlider image={product?.image_url} />
+      </div>
+
+      <div className="flex flex-col">
+        <div className="flex justify-between">
+          <div className="flex flex-col justify-end">
+            <h1 className="pb-4 font-bold">PRIMARK COLLECTION</h1>
+            <h1 className="text-lg font-small text-nowrap">{product?.name}</h1>
+          </div>
+          <div className="flex w-full items-center justify-center">
+            <p className="border p-3">Size</p>
+          </div>
+          <div className="flex items-center">
+            <Button primary name="Add to Cart" />
+          </div>
+        </div>
+        <ProductDetails price={product?.price} />
+        <div className="my-4">
+          <List headline="Shipping" details={SHIPPING_DETAILS} />
+        </div>
+      </div>
+    </div>
+  );
 }
