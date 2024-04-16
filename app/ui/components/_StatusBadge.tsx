dcc359ea3e6a45d09c01916624ef20f74000c0d7
corey <corey.cushnie@gmail.com>
2024-04-16 10:14:49 -0400

    update badge

--- app/ui/components/_StatusBadge.tsx
+++ app/ui/components/_StatusBadge.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { LuConstruction } from "react-icons/lu";
 
 export default function Status() {
   return (
-    <div className="border-2 border-black bg-amber-400 left-2 bottom-2 p-2 fixed z-50 rounded-full">
-      <h1 className="text-sm  ">In development</h1>
+    <div className="border-2 border-black bg-black flex space-x-2 left-2 items-center bottom-2 p-2 fixed z-50 rounded-full">
+      <LuConstruction color="yellow" />
+      <h1 className="text-sm text-white ">In development</h1>
+      <LuConstruction color="yellow" />
     </div>
   );
 }
