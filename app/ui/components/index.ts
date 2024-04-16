aa3031a41a8f666ec5c8b6337b6512d9eab2d2f6
corey <corey.cushnie@gmail.com>
2024-04-16 10:04:00 -0400

    update exports

--- app/ui/components/index.ts
+++ app/ui/components/index.ts
@@ -1,7 +1,20 @@
+import Status from "./_StatusBadge";
 import Navbar from "./navbar";
 import Banner from "./banner";
 import Button from "./button";
-
 import Logo from "./logo";
+import List from "./list";
+import ProductDetails from "./product-details";
 
-export { Navbar, Banner, Button, Logo };
+import { CardSlider } from "./card-slider";
+
+export {
+  Status,
+  Navbar,
+  Banner,
+  Button,
+  Logo,
+  CardSlider,
+  List,
+  ProductDetails,
+};
