22cea36abd7832587d7b049e47b6c2f1f2f43162
corey <corey.cushnie@gmail.com>
2024-04-16 10:03:31 -0400

    init badge

---
+++ app/ui/components/_StatusBadge.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Status() {
+  return (
+    <div className="border-2 border-black bg-amber-400 left-2 bottom-2 p-2 fixed z-50 rounded-full">
+      <h1 className="text-sm  ">In development</h1>
+    </div>
+  );
+}
