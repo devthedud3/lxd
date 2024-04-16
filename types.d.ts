09e02aa5d15383d9b3d0a8e9fbc481287f2f6629
corey <corey.cushnie@gmail.com>
2024-04-16 10:02:50 -0400

    types update

--- types.d.ts
+++ types.d.ts
@@ -2,3 +2,11 @@ type QueryType = {
   category: string;
   id: string | null;
 };
+
+type Item = {
+  name: string;
+  image_url: string;
+  price: string;
+  category: string;
+  subcategory: string;
+};
