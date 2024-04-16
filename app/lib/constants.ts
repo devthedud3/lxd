69401aba2dca24434d7dbb6fc1096c27b8a633cc
corey <corey.cushnie@gmail.com>
2024-04-16 10:03:04 -0400

    test data

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -14,3 +14,16 @@ export const SHOP_CATEGORIES = [
   "hats",
   "sneakers",
 ];
+
+export const TEST_DETAILS = [
+  "Sweatpants joggers are often made from a blend of materials such as cotton, polyester, and elastane. This blend provides a balance of comfort, durability, and flexibility.",
+  "They are designed to be soft and breathable, making them suitable for casual wear, workouts, or lounging at home.",
+  "Sweatpants joggers typically feature an elastic waistband with a drawstring closure, allowing for a comfortable and adjustable fit.",
+  "They often have a tapered design, meaning the legs are narrower at the ankles than at the thighs. This design enhances mobility and gives a more modern, streamlined look.",
+  "Many sweatpants joggers come with ribbed cuffs at the ankles. These cuffs help keep the pants in place and prevent them from riding up during activities.",
+];
+
+export const SHIPPING_DETAILS = [
+  "Standard processing time for orders take 5-7 business days to be processed and shipped.",
+  "Express processing time for orders take 1-3 business days to be processed and shipped.",
+];
