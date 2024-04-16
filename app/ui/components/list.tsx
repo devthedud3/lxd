0d2a92e8c40bed4611d633f4333b2edd6fcc4583
corey <corey.cushnie@gmail.com>
2024-04-16 10:04:08 -0400

    create list

---
+++ app/ui/components/list.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { RiArrowDropRightFill } from "react-icons/ri";
+
+type ButtonProps = {
+  headline?: string;
+  details: string[];
+};
+
+export default function List({ headline, details }: ButtonProps) {
+  return (
+    <div className="flex w-full flex-col">
+      <h1 className="font-medium py-2 w-full border-b text-sm">{headline}</h1>
+      <div className="flex flex-col ">
+        {details.map((text: string, key: number) => {
+          return (
+            <div className="flex items-center py-2" key={key}>
+              <RiArrowDropRightFill />
+              <p className="text-xs">{text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
