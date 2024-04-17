619080659b6f484c189795278e73e395c800d825
corey <corey.cushnie@gmail.com>
2024-04-17 11:15:13 -0400

    mobile web layout

--- app/ui/components/product-details.tsx
+++ app/ui/components/product-details.tsx
@@ -5,7 +5,7 @@ import { TEST_DETAILS } from "@/app/lib/constants";
 
 export default function ProductDetails({ price }: { price?: string }) {
   return (
-    <div className="flex flex-col h-full w-full justify-between">
+    <div className="flex flex-col h-full justify-between text-wrap">
       <p className="text-xs text-stone-400 py-3">${price}</p>
       <div className="h-24 border-l border-black" />
       <div className="my-4">
