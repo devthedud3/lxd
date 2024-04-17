ff692e89239f277c047c8cb8c9834ad4d95d43c6
corey <corey.cushnie@gmail.com>
2024-04-17 11:14:32 -0400

    center loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Loading({ show }: { show?: boolean }) {
   return (
-    <main className="flex h-screen w-full justify-center items-center">
-      <h2 className="text-primary">Loading</h2>
+    <main className="flex h-screen flex-col m-auto justify-center items-center">
+      <h2 className="text-primary text-xl">Loading</h2>
       <p>Hopefully not too long</p>
     </main>
   );
