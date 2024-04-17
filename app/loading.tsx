d319c59af3b961264f28618649a4a9fd8cdd5aa7
corey <corey.cushnie@gmail.com>
2024-04-16 22:16:32 -0400

    minor changes

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Loading() {
+export default function Loading({ show }: { show?: boolean }) {
   return (
     <main className="flex h-screen w-full justify-center items-center">
       <h2 className="text-primary">Loading</h2>
