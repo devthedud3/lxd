ce7d62f0d36acdd436599d9462976d3a4c4ab0ec
corey <corey.cushnie@gmail.com>
2024-04-16 22:17:35 -0400

    minor changes

--- app/ui/components/card-slider.tsx
+++ app/ui/components/card-slider.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Image from "next/image";
 
-export function CardSlider({ image }: { image?: string }) {
+export function CardSlider({
+  image,
+  height,
+}: {
+  image?: string;
+  height?: string;
+}) {
   return (
-    <>
-      <div className="flex w-full">
-        <Image
-          className="block"
-          src={image || "/homepage/logo.svg"}
-          alt={image || ""}
-          width={1000}
-          height={1000}
-          style={{ width: "auto" }}
-          priority
-        />
-      </div>
-    </>
+    <div className="flex w-full items-center justify-center">
+      <Image
+        className="block"
+        src={image || "/homepage/logo.svg"}
+        alt={image || ""}
+        width={1000}
+        height={1000}
+        style={{ width: "auto", height: `${height}` }}
+        priority
+      />
+    </div>
   );
 }
