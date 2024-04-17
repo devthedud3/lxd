70496d22fdea4b90d5ea513a04bfd60572f977d7
corey <corey.cushnie@gmail.com>
2024-04-16 22:17:05 -0400

    minor changes

---
+++ app/shop/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Banner, Footer, Navbar, Status } from "../ui/components";
+import Breadcrumbs from "../ui/components/breadcrumbs";
+
+export const metadata: Metadata = {
+  title: "LXD",
+  description: "Formerly known as CRWN",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className="md:px-32 ">
+        <Breadcrumbs />
+        {children}
+      </div>
+      <Footer />
+    </>
+  );
+}
