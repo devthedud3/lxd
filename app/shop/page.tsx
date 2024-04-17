36c79001c7b5df8bcf9870a0210072d5d24937c5
corey <corey.cushnie@gmail.com>
2024-04-16 22:17:09 -0400

    minor changes

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,13 +1,9 @@
-import { Footer } from "../ui/components";
 import { CardList } from "../ui/components/card-list";
 
 export default function Shop() {
   return (
-    <>
-      <div className="flex flex-1 flex-col h-full w-full items-center">
-        <CardList headline="find by category" search />
-      </div>
-      <Footer />
-    </>
+    <div className="flex flex-1 flex-col h-full w-full items-center">
+      <CardList headline="find by category" search />
+    </div>
   );
 }
