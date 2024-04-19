402dd3ea2106c3a483a8e1987ac4e18cddf01d18
corey <corey.cushnie@gmail.com>
2024-04-18 23:55:42 -0400

    login page

--- app/ui/components/navbar.tsx
+++ app/ui/components/navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { RiSearch2Line, RiShoppingCartLine } from "react-icons/ri";
 import Logo from "./logo";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
@@ -26,9 +27,12 @@ export default function Navbar() {
             className="transition ease-out duration-200 cursor-pointer hover:scale-110"
             size={20}
           /> */}
-          <p className="transition ease-out duration-200 border-b border-white hover:border-black hover:scale-105 cursor-pointer">
+          <Link
+            href="/login"
+            className="transition ease-out duration-200 border-b border-white hover:border-black hover:scale-105 cursor-pointer"
+          >
             Register
-          </p>
+          </Link>
         </div>
       </div>
     </div>
